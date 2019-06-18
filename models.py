d29dfc38dd1a0eb9b1afa50297c962c376b41a2a
theDogKnight <1123706238@qq.com>
2019-06-18 09:31:38 +0800

    add association among tags and media

--- models.py
+++ models.py
@@ -1,63 +1,66 @@
 from exts import db
 
-#char or string ?
+#String or string ?
 class Movies (db.Model):
     __tablename__ = 'movies'
     mo_id = db.Column(db.Integer(20),primary_key=True)
-    mo_English_name = db.Column(db.CHAR(225),nullable=False,index=True)
-    mo_name = db.Column(db.CHAR(255),nullable=False,index=True)
-    mo_introduction = db.Column(db.VARCHAR(1000),nullable=False)
-    mo_nation = db.Column(db.VARCHAR(40),nullable=False)
+    mo_English_name = db.Column(db.String(225),nullable=False,index=True)
+    mo_name = db.Column(db.String(255),nullable=False,index=True)
+    mo_introduction = db.Column(db.String(1000),nullable=False)
+    mo_nation = db.Column(db.String(40),nullable=False)
     mo_heat = db.Column(db.Integer(20),nullable=False)
-    mo_picture = db.Column(db.VARCHAR(5000),nullable=False)
+    mo_picture = db.Column(db.String(5000),nullable=False)
     mo_length = db.Column(db.Integer(10),nullable=False)
     mo_score = db.Column(db.Integer(5),nullable=False)
-    mo_date = db.Column(db.VARCHAR(20),nullable=False)
+    mo_date = db.Column(db.String(20),nullable=False)
     # mo_date = db.Column(db.Date,nullable=False)
 
 class Music (db.Model):
     __tablename__ = 'music'
     mu_id = db.Column(db.Integer(20),primary_key=True)
-    mu_name = db.Column(db.CHAR(255),nullable=False,index=True)
-    mu_introduction = db.Column(db.VARCHAR(10000),nullable=False)
-    mu_singer = db.Column(db.VARCHAR(100),nullable=False)
+    mu_name = db.Column(db.String(255),nullable=False,index=True)
+    mu_introduction = db.Column(db.String(10000),nullable=False)
+    mu_singer = db.Column(db.String(100),nullable=False)
     mu_heat = db.Column(db.Integer(20),nullable=False)
-    mu_picture = db.Column(db.VARCHAR(5000),nullable=False)
+    mu_picture = db.Column(db.String(5000),nullable=False)
     mu_score = db.Column(db.Integer(5),nullable=False)
-    mu_date = db.Column(db.VARCHAR(20),nullable=False)
+    mu_date = db.Column(db.String(20),nullable=False)
    # mu_date = db.Column(db.Date,nullable=False)
 
 
 class Book(db.Model):
     __tablename__ = 'book'
     b_id = db.Column(db.Integer(20),primary_key=True)
-    b_name = db.Column(db.CHAR(255),nullable=False,index=True)
-    b_introduction = db.Column(db.VARCHAR(10000),nullable=False)
-    b_pulishing_house = db.Column(db.VARCHAR(500),nullable=False)
-    b_writer = db.Column(db.VARCHAR(100),nullable=False)
+    b_name = db.Column(db.String(255),nullable=False,index=True)
+    b_introduction = db.Column(db.String(10000),nullable=False)
+    b_pulishing_house = db.Column(db.String(500),nullable=False)
+    b_writer = db.Column(db.String(100),nullable=False)
     b_score = db.Column(db.Integer(5),nullable=False)
     b_heat = db.Column(db.Integer(20),nullable=False)
-    b_date = db.Column(db.VARCHAR(20),nullable=False)
+    b_date = db.Column(db.String(20),nullable=False)
 
 
 class User(db.Model):
     __tablename = 'user'
     u_id = db.Column(db.Integer(20),primary_key=True)
-    u_name = db.Column(db.CHAR(40),nullable=False,index=True)
-    u_image = db.Column(db.VARCHAR(1000),nullable=False)
+    u_name = db.Column(db.String(40),nullable=False,index=True)
+    u_image = db.Column(db.String(1000),nullable=False)
     #todo
 
 class Tag(db.Model):
     __tablename__ = 'tag'
     t_id = db.Column(db.Integer(20),primary_key=True)
-    t_tag = db.Column(db.VARCHAR(100),nullable=False)
+    t_tag = db.Column(db.String(100),nullable=False)
+    movies = relationship("movie", secondary=tag_associate_movie, backref="TagstoMovie")
+    books = relationship("book", secondary=tag_associate_book, backref="TagstoBook")
+    music = relationship("music", secondary=tag_associate_music, backref="TagstoMusic")
 
 
 class User_Comment_Movie(db.Model):
     __tablename__ = 'userComMovie'
     u_id = db.Column(db.Integer(20),primary_key=True)
     mo_id = db.Column(db.Integer(20),primary_key=True)
-    mo_comment = db.Column(db.VARCHAR(10000),nullable=False)
+    mo_comment = db.Column(db.String(10000),nullable=False)
     mo_score = db.Column(db.Integer(5),nullable=False)
 
 
@@ -72,7 +75,7 @@ class User_Comment_Music(db.Model):
     __tablename__ = 'userComMusic'
     u_id = db.Column(db.Integer(20),primary_key=True)
     mu_id = db.Column(db.Integer(20),primary_key=True)
-    mu_comment = db.Column(db.VARCHAR(10000),nullable=False)
+    mu_comment = db.Column(db.String(10000),nullable=False)
     mu_score = db.Column(db.Integer(5),nullable=False)
 
 
@@ -88,7 +91,7 @@ class User_Comment_Book(db.Model):
     __tablename__ = 'userComBook'
     u_id = db.Column(db.Integer(20),primary_key=True)
     b_id = db.Column(db.Integer(20),primary_key=True)
-    b_comment = db.Column(db.VARCHAR(10000),nullable=False)
+    b_comment = db.Column(db.String(10000),nullable=False)
     mb_score = db.Column(db.Integer(5),nullable=False)
 
 
@@ -103,3 +106,16 @@ class User_Like_Tags(db.Model):
     __tablename__ = 'userLikeTags'
     u_id = db.Column(db.Integer(20),primary_key=True)
     t_id = db.Column(db.Integer(20),primary_key=True)
+
+tag_associate_book = db.Table('tagToBook',db.metadata,
+                              db.Column('t_id',db.Integer(20),db.ForeignKey('tag.t_id')),
+                              db.Column('b_id',db.Integer(20),db.ForeignKey('book.b_id'))
+                              )
+tag_associate_movie = db.Table('tagToMovie',db.metadata,
+                              db.Column('t_id',db.Integer(20),db.ForeignKey('tag.t_id')),
+                              db.Column('mo_id',db.Integer(20),db.ForeignKey('movie.mo_id'))
+                              )
+tag_associate_music = db.Table('tagToMusic',db.metadata,
+                              db.Column('t_id',db.Integer(20),db.ForeignKey('tag.t_id')),
+                              db.Column('mu_id',db.Integer(20),db.ForeignKey('music.mu_id'))
+                              )
